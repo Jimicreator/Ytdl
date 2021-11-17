f177674776616686d96e382d9af6f573301bc618
Jimicreator <94049086+Jimicreator@users.noreply.github.com>
2021-11-17 22:12:44 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -8,5 +8,5 @@ async def start(client, message):
 		[InlineKeyboardButton("Report Bugs 🚨", url="https://t.me/Jimibots_grp")]
                 ])
 
-	help_image = "https://telegra.ph/file/7228b162849378a99a635.jpg"
-	await message.reply_photo(help_image,  caption="**💡 HELP 📃...**\n \n__• Just Send your Youtube video url 🌟__ \n__• And i will give Method list to select your choice 😋__\n \n======================\n__• 😊 This bot is fully free.__\n`•⚙ Don't pay anyone for Bots like this.`\n\n⚡ ```Bot By Jimi BOTS``` 🚨\n",reply_markup=alpha2)
+	help_image = "https://telegra.ph/file/1e0a3145c7273f446b000.jpg"
+	await message.reply_photo(help_image,  caption="**💡 HELP 📃...**\n \n__• Just Send your Youtube video url 🌟__ \n__• And i will give Method list to select your choice 😋__\n \n======================\n__• 😊 This bot is fully free.__\n`•⚙ Don't pay anyone for Bots like this.`\n\n⚡ ```Bot By @Jimi_Bots``` 🚨\n",reply_markup=alpha2)
