b93931a444e0e3b301c45f86837c3adad46e0fa0
Jimicreator <94049086+Jimicreator@users.noreply.github.com>
2021-11-17 22:15:55 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,10 +6,10 @@ async def start(client, message):
     # return
     Alpha = InlineKeyboardMarkup([
         
-        [InlineKeyboardButton("📌️ JNS BOTS 🔎", url="https://t.me/JNS_BOTS")],
-        [InlineKeyboardButton("📌️ support 🔎", url="https://t.me/JNS_FC_BOTS")]
+        [InlineKeyboardButton("🤓 Jimi Bots 🔎", url="https://t.me/Jimi_Bots")],
+        [InlineKeyboardButton("😎 support 🔎", url="https://t.me/jimibots_grp")]
 
     ])
-    thumbnail_url = "https://telegra.ph/file/7228b162849378a99a635.jpg"
-    await message.reply_photo(thumbnail_url, caption=f"**🙂 Hi <b>{message.from_user.first_name}**</b>\n\n<br>__😇 I Can Download YT Videos For You ✨️__</br>\n\n<b>• **🗂️ Instructions for use...🚨**</b>\n• **⚙ Type /help to get instructins...**\n \n───── ❝ **Lets Play** ❞ ─────\n ", reply_markup=Alpha)
+    thumbnail_url = "https://telegra.ph/file/1e0a3145c7273f446b000.jpg"
+    await message.reply_photo(thumbnail_url, caption=f"**🙂 Hi <b>{message.from_user.first_name}**</b>\n\n<br>__😇 I Can Download YT Files For You ✨️__</br>\n\n<b>• **🗂️ Instructions for use...🚨**</b>\n• **⚙ Type /help to get instructins...**\n \n───── ❝ **Lets Play** ❞ ─────\n ", reply_markup=Alpha)
     raise StopPropagation
